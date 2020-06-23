9a3cc5eeb3731ebc85544bcc8fc585b16aa429a7
FelipeA0 <gonzalez.aegf@gmail.com>
2020-06-23 03:56:53 -0500

    nueva conexion nodejs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,18 +14,18 @@ export class AppComponent {
   }
   pruebas(): void {
     this.apisService
-      .consulta('http://localhost:3000/user')
+      .consulta('http://node2-env.eba-cbx2nejh.us-east-2.elasticbeanstalk.com/user')
       .subscribe((res: any) => console.log(res));
     let id = 7; //id a consultar
     this.apisService
-      .consultaPorId(`http://localhost:3000/user/${id}`)
+      .consultaPorId(`http://node2-env.eba-cbx2nejh.us-east-2.elasticbeanstalk.com/user/${id}`)
       .subscribe((res: any) => console.log(res));
     let name = 'Luisa';
     let lastname = 'Alavarez';
     let contact = 'luisa@live.com';
     let cellphone = '582169';
     this.apisService
-      .alta('http://localhost:3000/user', name, lastname, contact, cellphone)
+      .alta('http://node2-env.eba-cbx2nejh.us-east-2.elasticbeanstalk.com/user', name, lastname, contact, cellphone)
       .subscribe((res: any) => console.log(res));
     let body = {
       name: 'Salvador',
@@ -34,7 +34,7 @@ export class AppComponent {
       cellphone: '98753',
     };
     this.apisService
-      .alta2('http://localhost:3000/user', body)
+      .alta2('http://node2-env.eba-cbx2nejh.us-east-2.elasticbeanstalk.com/user', body)
       .then((data) => {
         console.log(data);
       })
